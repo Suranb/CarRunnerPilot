f6d155b5380df46ff1b7034a788a47379bc2da95
Suranb <Suranbasharati@gmail.com>
2023-08-06 17:52:45 +0200

    added DoTween

--- EndlessCarRunner/Assets/Scripts/Core/PlayerController.cs
+++ EndlessCarRunner/Assets/Scripts/Core/PlayerController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace CarRunner.Player
@@ -5,31 +6,55 @@ namespace CarRunner.Player
   public class PlayerController : MonoBehaviour
   {
     public float laneOffset = 3f; // The distance between each lane
-    public float laneSwitchSpeed = 10f; // Speed of lane switching
+    public float laneSwitchSpeed = 2f; // Speed of lane switching
     public int currentLane = 1; // The current lane (0 for left, 1 for middle, 2 for right)
     private Vector3 targetPosition; // The position to which the car will move
     private bool hasCollided = false;
+    private bool isLaneSwitching = false;
+
+
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private Quaternion targetRotation;
+    [SerializeField] private Quaternion initialRotation;
+    [SerializeField] private float laneChangeRotation = 15f;
+
     private void Start()
     {
       targetPosition = transform.position;
+      initialRotation = transform.rotation;
+      targetRotation = initialRotation;
     }
 
     private void Update()
     {
-      // Detect lane switch input (for simplicity, use keyboard input here, but you can adapt it to touch controls for mobile)
+      // Detect lane switch input
       if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLane > 0)
       {
+        isLaneSwitching = true;
         currentLane--;
         targetPosition += Vector3.left * laneOffset;
+        targetRotation = Quaternion.Euler(initialRotation.eulerAngles.x, initialRotation.eulerAngles.y, initialRotation.eulerAngles.z - laneChangeRotation);
       }
       else if (Input.GetKeyDown(KeyCode.RightArrow) && currentLane < 2)
       {
+        isLaneSwitching = true;
         currentLane++;
         targetPosition += Vector3.right * laneOffset;
+        targetRotation = Quaternion.Euler(initialRotation.eulerAngles.x, initialRotation.eulerAngles.y, initialRotation.eulerAngles.z + laneChangeRotation);
       }
 
+      // If the car is close to its target position and was previously lane-switching, start transitioning back to the initial rotation
+      if (Vector3.Distance(transform.position, targetPosition) < 1f && isLaneSwitching)
+      {
+        isLaneSwitching = false;
+        targetRotation = initialRotation;
+      }
+
+      // Rotate the car
+      transform.DORotateQuaternion(targetRotation, rotationSpeed * Time.deltaTime);
+
       // Move the car smoothly towards the target position
-      transform.position = Vector3.Lerp(transform.position, targetPosition, laneSwitchSpeed * Time.deltaTime);
+      transform.DOMove(targetPosition, laneSwitchSpeed * Time.deltaTime).SetEase(Ease.InSine);
     }
 
     private void OnCollisionEnter(Collision collision)
